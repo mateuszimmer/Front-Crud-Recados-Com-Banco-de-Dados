59fefe7d46d19084db3bcac30baaffa6478f7b48
Mateus Zimmer <102668946+mateuszimmer@users.noreply.github.com>
2023-03-04 00:02:01 -0300

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     // baseURL: 'http://localhost:8080',
-    baseURL: 'https://api-crud-recados-com-banco-de-dados.vercel.app/',
+    baseURL: 'https://api-crud-recados-com-banco-de-dados.vercel.app',
     headers: {
         "Content-type": "application/json"
     },
